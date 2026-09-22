2b9c03d1227eb84be9a292891b57a0fac45feab6
ConnorFoutz <fou25005@byui.edu>
2026-09-22 17:54:04 -0600

    C# prep 2 complete

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string sgrade;
+        string letter;
+
+        Console.WriteLine("Welcome to the grade calculator");
+        Console.Write("Please enter your grade: ");
+        sgrade = Console.ReadLine();
+
+        int grade = int.Parse(sgrade);
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"You got an {letter}.");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("You passed! Good job.");
+        }
+        else
+            Console.WriteLine("You failed. Keep trying!");
+
+
+
+
     }
 }
